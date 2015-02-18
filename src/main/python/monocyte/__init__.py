2b4a5190c32fecbc4aaba6527e1f7f755e773f3b
Jan Brennenstuhl <jan.brennenstuhl@immobilienscout24.de>
2015-02-18 16:24:35 +0100

    start upper case just because it's nicer.

--- src/main/python/monocyte/__init__.py
+++ src/main/python/monocyte/__init__.py
@@ -7,7 +7,7 @@ class Monocyte(object):
 
     def search_and_destroy_unwanted_resources(self):
         for handler_cls in aws_handler.all:
-            print("\nchecking %s resources" % handler_cls.SERVICE_NAME)
+            print("\nChecking %s resources" % handler_cls.SERVICE_NAME)
             handler = handler_cls()
             handler.fetch_all_resources()
 
