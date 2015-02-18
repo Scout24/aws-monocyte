a7bc301cc4bd63b263d55af5b1ffb381b08b46c0
Arne Hilmann <arne@hilmann.de>
2015-02-18 13:59:42 +0100

    refactored: extract service handlers in own classes

--- src/main/python/monocyte/__init__.py
+++ src/main/python/monocyte/__init__.py
@@ -1,75 +1,12 @@
 from __future__ import print_function
 
-import boto
-
+from monocyte.handler import aws_handler
 
 class Monocyte(object):
 
-    OK = 0
-
-    def __init__(self, blacklisted=None):
-#        self.services = ['awslambda', 'beanstalk', 'cloudformation', 'cloudformation', 'cloudhsm', 'cloudsearch',
-#                         'cloudsearch2', 'cloudsearchdomain', 'cloudtrail', 'codedeploy', 'cognito.identity',
-#                         'cognito.sync', 'configservice', 'datapipeline', 'dynamodb', 'dynamodb2', 'ec2.autoscale',
-#                         'ec2.cloudwatch', 'ec2.elb', 'ec2', 'ec2.containerservice', 'elasticache', 'elastictranscoder',
-#                         'emr', 'glacier', 'iam', 'kinesis', 'kms', 'logs', 'opsworks', 'rds', 'rds2', 'redshift',
-#                         'route53.domains', 's3', 'sdb', 'ses', 'sns', 'sqs', 'sts', 'support', 'swf', 'vpc']
-        self.services = ["ec2"]
-        self.blacklisted = blacklisted if blacklisted else ['eu-west-1', 'eu-central-1']
-
-        print('Blacklisted regions: [{0}]'.format(', '.join(self.blacklisted)))
-
-    def get_dumper(self, module_name):
-        default_dumper = lambda instance: print("instance found in {region.name}\n\t{id}".format(instance))
-        instance_dumpers = {
-                "boto.ec2": lambda instance: print("instance found in {region.name}\n\t{id} [{image_id}] - {instance_type}, since {launch_time}\n\tip {public_dns_name}, key {key_name}".format(**vars(instance)))
-        }
-        return instance_dumpers.get(module_name, default_dumper)
-
     def search_and_destroy_unwanted_resources(self):
-        for service in self.services:
-            boto_module, regions = self._fetch_regions_by_service(service)
-            if not regions:
-                continue
-            for region in regions:
-                if region.name in self.blacklisted:
-                    #print("\tBlacklisted, thus skipped")
-                    continue
-                resources_in_region = self._fetch_resources_by_region(boto_module, region)
-                #self._destroy_resources(resources_in_region)
+        for _, handler_cls in aws_handler.all.iteritems():
+            print("\nchecking %s resources" % handler_cls.SERVICE_NAME)
+            handler = handler_cls()
+            handler.fetch_all_resources()
 
-        return Monocyte.OK
-
-    def _fetch_regions_by_service(self, service):
-        regions = None
-        module_identifier = 'boto.%s' % service
-
-        try:
-            boto_module = __import__(module_identifier, globals(), locals(), [service])
-            regions = boto_module.regions()
-            regions = regions if regions else []
-            print('Fetched regions for {0}: {1}'.format(service, len(regions)))
-        except ImportError:
-            print('Could not import ' + module_identifier)
-            raise
-
-        return (boto_module, regions)
-
-    def _fetch_resources_by_region(self, mod, region):
-        connection = mod.connect_to_region(region.name)
-        instances = []
-
-        dumper = self.get_dumper(mod.__name__)
-        try:
-            instances = connection.get_only_instances()
-            for instance in instances:
-                dumper(instance)
-        except BaseException as e:
-            print(e)
-            raise
-
-        return instances
-
-    def _destroy_resources(self, resources):
-        # TODO implement destroying resources
-        return 0
---
+++ src/main/python/monocyte/handler.py
@@ -0,0 +1,71 @@
+import boto
+import boto.ec2
+
+def is_region_allowed(region):
+    return region.lower().startswith("eu")
+
+def is_region_ignored(region):
+    return region.lower() in ["cn-north-1", "us-gov-west-1"]
+
+def make_registrar():
+    registry = {}
+
+    def registrar(cls):
+        registry[cls.SERVICE_NAME] = cls
+        print("registering aws service %s" % cls.SERVICE_NAME)
+        return cls
+
+    registrar.all = registry
+    return registrar
+
+aws_handler = make_registrar()
+
+
+@aws_handler
+class EC2(object):
+    SERVICE_NAME = "ec2"
+
+    def __init__(self):
+        regions = boto.ec2.regions() or []
+        self.regions = [region for region in regions
+                if not is_region_allowed(region.name) and not is_region_ignored(region.name)]
+
+    def fetch_all_resources(self):
+        for region in self.regions:
+            connection = boto.ec2.connect_to_region(region.name)
+            resources = self.fetch_all_resources_in_region(connection) or []
+            for resource in resources:
+                print("ec2 instance found in {region.name} -> MUST BE REMOVED\n\t{id} [{image_id}] - {instance_type}, since {launch_time}\n\tip {public_dns_name}, key {key_name}".format(**vars(resource)))
+
+    def fetch_all_resources_in_region(self, connection):
+        try:
+            return connection.get_only_instances()
+        except BaseException as e:
+            print(e)
+            print(connection)  # TODO more infos here
+
+    def delete(self, instance):
+        pass
+
+
+@aws_handler
+class S3(object):
+    SERVICE_NAME = "s3"
+
+    def fetch_all_resources(self):
+        connection = boto.connect_s3()
+        for bucket in connection.get_all_buckets():
+            try:
+                location = bucket.get_location()
+            except:
+                location = "__unknown__"
+            if not is_region_allowed(location):
+                print("s3 bucket found in {0} -> {1}\n\t{2}, created {3}".format(
+                    location,
+                    "okay" if is_region_allowed(location) else "MUST BE REMOVED!",  # TODO more generic
+                    bucket.name,
+                    bucket.creation_date))
+
+    def delete(self, instance):
+        pass
+
