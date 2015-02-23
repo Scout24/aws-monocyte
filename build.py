376a5181bdf643a2be190b4c79104fa1afded950
Domme, Sergej <sergej.domme@immobilienscout24.de>
2015-02-23 09:06:40 +0100

    need mock for unit tests

--- build.py
+++ build.py
@@ -33,6 +33,8 @@ def set_properties(project):
     project.depends_on("docopt")
     project.depends_on("boto")
 
+    project.build_depends_on("mock")
+
     project.set_property("flake8_include_test_sources", True)
     project.set_property('coverage_break_build', False)
 
