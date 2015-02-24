06acc592d18a8422afe54782703e9ee161842599
Jan Brennenstuhl <jan.brennenstuhl@immobilienscout24.de>
2015-02-24 13:20:49 +0100

    PEP8 fixes.

--- src/main/python/monocyte/__init__.py
+++ src/main/python/monocyte/__init__.py
@@ -3,8 +3,9 @@ from __future__ import print_function
 from monocyte.handler import aws_handler
 
 REMOVE_WARNING = "WARNING: region '%s' not allowed!"
-IGNORED_REGIONS = ["cn-north-1", "us-gov-west-1","us-east-1"]
+IGNORED_REGIONS = ["cn-north-1", "us-gov-west-1", "us-east-1"]
 ALLOWED_REGIONS_STARTS_WITH = "eu"
+
 
 class Monocyte(object):
 
@@ -17,7 +18,7 @@ class Monocyte(object):
     def is_region_handled(self, region):
         return self.is_region_allowed(region) or not self.is_region_ignored(region)
 
-    def search_and_destroy_unwanted_resources(self, dry_run=True ):
+    def search_and_destroy_unwanted_resources(self, dry_run=True):
         if dry_run:
             print(" DRY RUN " * 8)
 
@@ -28,10 +29,10 @@ class Monocyte(object):
 
         for handler_cls in aws_handler.all:
             print("\n---- checking %s resources" % handler_cls.SERVICE_NAME)
-            handler = handler_cls(self.is_region_handled, dry_run)
-            for resource in handler.fetch_all_resources():
+            specific_handler = handler_cls(self.is_region_handled, dry_run)
+            for resource in specific_handler.fetch_all_resources():
                 if not self.is_region_allowed(resource.region):
                     print("\n%s\n\t%s" % (
-                        handler.to_string(resource),
+                        specific_handler.to_string(resource),
                         REMOVE_WARNING % resource.region))
-                    handler.delete(resource)
+                    specific_handler.delete(resource)
