d38fd28c47f3749ed3fb7a64827768108a413c78
Arne Hilmann <arne@hilmann.de>
2015-02-26 13:23:47 +0100

    introduce ec2.volume handling

--- src/main/python/monocyte/handler/ec2.py
+++ src/main/python/monocyte/handler/ec2.py
@@ -7,13 +7,13 @@ from monocyte.handler import Resource, aws_handler
 
 
 @aws_handler
-class Handler(object):
+class Instance(object):
     VALID_TARGET_STATES = ["terminated", "shutting-down"]
 
     def __init__(self, region_filter, dry_run=True):
         self.regions = [region for region in boto.ec2.regions() if region_filter(region.name)]
         self.dry_run = dry_run
-        self.name = __name__.rsplit(".", 1)[1]
+        self.name = "ec2.instance"
         self.order = 2
 
     def fetch_unwanted_resources(self):
@@ -29,9 +29,9 @@ class Handler(object):
                "\n\tdnsname {public_dns_name}, key {key_name}, state {_state}".format(**vars(resource.wrapped))
 
     def delete(self, resource):
-        if resource.wrapped.state in Handler.VALID_TARGET_STATES:
+        if resource.wrapped.state in Instance.VALID_TARGET_STATES:
             print("\tstate '{}' is a valid target state ({}), skipping".format(
-                resource.wrapped.state, ", ".join(Handler.VALID_TARGET_STATES)))
+                resource.wrapped.state, ", ".join(Instance.VALID_TARGET_STATES)))
             return []
         connection = boto.ec2.connect_to_region(resource.region)
         if self.dry_run:
@@ -46,3 +46,26 @@ class Handler(object):
             instances = connection.terminate_instances([resource.wrapped.id], dry_run=False)
             print("\tInitiating shutdown sequence for {0}".format(instances))
             return instances
+
+
+@aws_handler
+class Volume(object):
+    def __init__(self, region_filter, dry_run=True):
+        self.regions = [region for region in boto.ec2.regions() if region_filter(region.name)]
+        self.dry_run = dry_run
+        self.name = "ec2.volume"
+        self.order = 3
+
+    def fetch_unwanted_resources(self):
+        for region in self.regions:
+            connection = boto.ec2.connect_to_region(region.name)
+            resources = connection.get_all_volumes() or []
+            for resource in resources:
+                yield Resource(resource, region.name)
+
+    def to_string(self, resource):
+        return "ebs volume found in {region.name}\n\t" \
+               "{id} {status}, since {create_time}".format(**vars(resource.wrapped))
+
+    def delete(self, resource):
+        pass
