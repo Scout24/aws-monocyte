83b0130f758581b979da3902bd07e41a8c1ccbe7
Sergej Domme <sergej.domme@immobilienscout24.de>
2015-02-27 15:19:28 +0100

    start to unittest cloudformation module

---
+++ src/unittest/python/handler/cloudformation_tests.py
@@ -0,0 +1,38 @@
+import boto.cloudformation
+import boto.cloudformation.stack
+
+from unittest import TestCase
+from mock import patch, Mock
+from monocyte.handler import cloudformation
+
+
+class CloudFormationTest(TestCase):
+
+    def setUp(self):
+        self.boto_mock = patch("monocyte.handler.cloudformation.boto").start()
+        self.positive_fake_region = Mock(boto.cloudformation.regions)
+        self.positive_fake_region.name = "allowed_region"
+        self.negative_fake_region = Mock(boto.cloudformation.regions)
+        self.negative_fake_region.name = "forbbiden_region"
+        self.boto_mock.cloudformation.regions.return_value = [self.positive_fake_region, self.negative_fake_region]
+
+        self.cloudformation_handler_filter = cloudformation.Handler(lambda region_name: region_name == self.positive_fake_region.name)
+
+        self.stack_mock = self._given_stack_mock()
+
+    def tearDown(self):
+        patch.stopall()
+
+    def test_fetch_unwanted_resources_filtered(self):
+        only_resource = list(self.cloudformation_handler_filter.fetch_unwanted_resources())[0]
+        self.assertEquals(only_resource.wrapped, self.stack_mock)
+
+    def _given_stack_mock(self):
+        stack_mock = Mock(boto.cloudformation.stack.Stack, stack_name="test-stack")
+        stack_mock.stack_id = "id-12345"
+        stack_mock.stack_status = "CREATE_COMPLETE"
+
+        self.boto_mock.cloudformation.connect_to_region.return_value.valid_states = ('CREATE_COMPLETE',
+                                                                                     'DELETE_COMPLETE')
+        self.boto_mock.cloudformation.connect_to_region.return_value.list_stacks.return_value = [stack_mock]
+        return stack_mock
