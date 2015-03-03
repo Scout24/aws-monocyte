71db312bbda7490e6f1723231d2b6b827d735c38
Sergej Domme <sergej.domme@immobilienscout24.de>
2015-03-03 09:19:57 +0100

    add Licence

--- src/unittest/python/handler/cloudformation_tests.py
+++ src/unittest/python/handler/cloudformation_tests.py
@@ -1,3 +1,18 @@
+# Monocyte - An AWS Resource Destroyer
+# Copyright 2015 Immobilien Scout GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import boto.cloudformation
 import boto.cloudformation.stack
 
@@ -17,7 +32,7 @@ class CloudFormationTest(TestCase):
         self.negative_fake_region.name = "forbbiden_region"
         self.boto_mock.cloudformation.regions.return_value = [self.positive_fake_region, self.negative_fake_region]
 
-        self.cloudformation_handler_filter = cloudformation.Handler(lambda region_name: region_name == self.positive_fake_region.name)
+        self.cloudformation_handler_filter = cloudformation.Stack(lambda region_name: region_name == self.positive_fake_region.name)
 
         self.stack_mock = self._given_stack_mock()
 
