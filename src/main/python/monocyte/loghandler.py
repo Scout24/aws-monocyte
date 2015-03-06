191b25f459c08ea8da5032ac8153375e4e70d454
Arne Hilmann <arne@hilmann.de>
2015-03-06 14:09:34 +0100

    add loghandler for CloudWatchLogs

---
+++ src/main/python/monocyte/loghandler.py
@@ -0,0 +1,56 @@
+import json
+import logging
+
+import boto.logs
+from boto.logs.exceptions import (DataAlreadyAcceptedException,
+                                  InvalidSequenceTokenException,
+                                  ResourceAlreadyExistsException)
+
+
+class CloudWatchHandler(logging.StreamHandler):
+
+    def __init__(self, region, log_group_name, log_stream_name, level=logging.INFO):
+        super(CloudWatchHandler, self).__init__(level)
+        self.region = region
+        self.log_group_name = log_group_name
+        self.log_stream_name = log_stream_name
+        self.connection = boto.logs.connect_to_region(region)
+        self.sequence_token = None
+        self.create_group_and_stream(log_group_name, log_stream_name)
+
+    def create_group_and_stream(self, log_group_name, log_stream_name):
+        try:
+            self.connection.create_log_group(log_group_name)
+        except ResourceAlreadyExistsException:
+            pass
+        try:
+            self.connection.create_log_stream(log_group_name, log_stream_name)
+        except ResourceAlreadyExistsException:
+            pass
+
+    def _put_message(self, message, timestamp):
+        event = {"message": message, "timestamp": timestamp}
+        result = self.connection.put_log_events(
+                self.log_group_name,
+                self.log_stream_name,
+                [event],
+                self.sequence_token)
+        self.sequence_token = result.get("nextSequenceToken", None)
+
+    def put_message(self, message, timestamp):
+        try:
+            self._put_message(message, timestamp)
+        except (DataAlreadyAcceptedException, InvalidSequenceTokenException) as e:
+            if e.status != 400:
+                raise
+            next_sequence_token = e.body.get("expectedSequenceToken", None)
+            if next_sequence_token:
+                self.sequence_token = next_sequence_token
+                self._put_message(message, timestamp)
+            else:
+                raise
+
+    def emit(self, record):
+        timestamp = int(record.created * 1000)
+        message = json.dumps(vars(record))
+        self.put_message(message, timestamp)
---
+++ src/unittest/python/loghandler_tests.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+import logging
+
+from mock import patch
+import unittest
+
+import boto
+
+from monocyte.loghandler import CloudWatchHandler
+
+
+class CloudWatchHandlerTest(unittest.TestCase):
+
+    def setUp(self):
+        self.boto_mock = patch("monocyte.loghandler.boto").start()
+        self.connection = self.boto_mock.logs.connect_to_region.return_value
+        self.logger = logging.getLogger("Test")
+        self.logger.setLevel(logging.WARN)
+        # # do you want a console output, too? use this...
+        # console_handler = logging.StreamHandler(sys.stdout)
+        # console_handler.setFormatter(logging.Formatter('%(levelname)-8s %(message)s'))
+        # self.logger.addHandler(console_handler)
+
+    def test_cloudwatch_logging(self):
+        cloudwatch_handler = CloudWatchHandler("eu-central-1", "monocyte", "test")
+        self.logger.addHandler(cloudwatch_handler)
+
+        self.connection.create_log_group.assert_called_with("monocyte")
+        self.connection.create_log_stream.assert_called_with("monocyte", "test")
+
+        self.logger.debug("aha")
+        self.logger.info("aha")
+        self.assertFalse(self.connection.put_log_events.called)
+
+        self.logger.warn("aha")
+        call_args = self.connection.put_log_events.call_args[0]
+        self.assertEqual("monocyte", call_args[0])
+        self.assertEqual("test", call_args[1])
+        log_event = call_args[2][0]
+        self.assertIn("timestamp", log_event)
+        self.assertIn("message", log_event)
+        self.assertIn("WARNING", log_event["message"])
+        self.assertIn("aha", log_event["message"])
+
+    def test_log_group_already_exists(self):
+        e = boto.logs.exceptions.ResourceAlreadyExistsException(400, "binschonda")
+        self.connection.create_log_group.side_effect = e
+        self.connection.create_log_stream.side_effect = e
+        CloudWatchHandler("eu-central-1", "monocyte", "test")
+
+    def test_response_with_invalid_sequence_token(self):
+        e = boto.logs.exceptions.InvalidSequenceTokenException(400, "foo")
+        e.body = {"expectedSequenceToken": "correct_token"}
+        self.connection.put_log_events.side_effect = e
+
+        handler = CloudWatchHandler("eu-central-1", "monocyte", "test")
+        self.assertRaises(boto.logs.exceptions.InvalidSequenceTokenException,
+                          handler.put_message, "message", "timestamp")
+
+    def test_invalid_response(self):
+        e = boto.logs.exceptions.InvalidSequenceTokenException(500, "completelybroken")
+        self.connection.put_log_events.side_effect = e
+
+        handler = CloudWatchHandler("eu-central-1", "monocyte", "test")
+        self.assertRaises(boto.logs.exceptions.InvalidSequenceTokenException,
+                          handler.put_message, "message", "timestamp")
+
+    def test_response_with_missing_sequence_token(self):
+        e = boto.logs.exceptions.InvalidSequenceTokenException(400, "foo")
+        e.body = {}
+        self.connection.put_log_events.side_effect = e
+
+        handler = CloudWatchHandler("eu-central-1", "monocyte", "test")
+        self.assertRaises(boto.logs.exceptions.InvalidSequenceTokenException,
+                          handler.put_message, "message", "timestamp")
+
+
+if __name__ == "__main__":
+    unittest.main()
