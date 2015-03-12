b67cf1621829cbe8e56b1982fd18019469fa07a7
Jan Brennenstuhl <jan.brennenstuhl@immobilienscout24.de>
2015-03-12 14:04:06 +0100

    refactors messages for proper logging.

--- src/main/python/monocyte/handler/dynamodb.py
+++ src/main/python/monocyte/handler/dynamodb.py
@@ -30,26 +30,25 @@ class Table(Handler):
     def fetch_unwanted_resources(self):
         for region in self.regions:
             connection = boto.dynamodb2.connect_to_region(region.name)
-            names = connection.list_tables(limit=100) or {}  # TODO what happens with more than 100 tables per region?
+            names = connection.list_tables(limit=100) or {}  # TODO ahilmann: what happens with more than 100 tables per region?
             for name in names.get("TableNames"):
                 resource = connection.describe_table(name)
                 yield Resource(resource["Table"], region.name)
 
     def to_string(self, resource):
         table = resource.wrapped
-        return "DynamoDB Table found in {0}".format(resource.region) + \
-               "\n\t{0}, since {1}, state {2}".format(
+        return "DynamoDB Table found in {0}, ".format(resource.region) + \
+               "with name {0}, created {1}, with state {2}".format(
                    table["TableName"],
                    datetime.datetime.fromtimestamp(table["CreationDateTime"]).strftime('%Y-%m-%d %H:%M:%S.%f'),
                    table["TableStatus"])
 
     def delete(self, resource):
         if self.dry_run:
-            self.logger.info("\tTable would be removed")
             return
         connection = boto.dynamodb2.connect_to_region(resource.region)
-        self.logger.info("\tInitiating deletion sequence")
+        self.logger.info("Initiating deletion sequence for {0}.".format(resource.wrapped["TableName"]))
         try:
             connection.delete_table(resource.wrapped["TableName"])
         except boto.dynamodb2.exceptions.ResourceInUseException as exc:
-            self.logger.info("\t{0}".format(exc))
+            self.logger.exception(exc)
