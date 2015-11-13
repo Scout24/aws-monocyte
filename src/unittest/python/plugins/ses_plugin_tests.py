34e692934c7e502b206c1f2cb2b5b11db50cd439
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2015-11-13 11:20:42 +0100

    PIO-124 Test that email is actually sent

--- src/unittest/python/plugins/ses_plugin_tests.py
+++ src/unittest/python/plugins/ses_plugin_tests.py
@@ -1,6 +1,9 @@
 from __future__ import print_function, absolute_import, division
 
 from unittest import TestCase
+from moto import mock_ses
+import boto
+
 from monocyte.plugins.ses_plugin import AwsSesPlugin
 
 
@@ -20,3 +23,15 @@ class AwsSesPluginTest(TestCase):
         self.assertEqual(self.aws_ses_plugin.sender, self.sender)
         self.assertEqual(self.aws_ses_plugin.recipients, self.recipients)
         self.assertEqual(self.aws_ses_plugin.body, self.body)
+
+    @mock_ses
+    def test_should_send_mail(self):
+        conn = boto.connect_ses('the_key', 'the_secret')
+        conn.verify_email_identity("thomas.lehmann@immobilienscout24.de")
+
+        self.aws_ses_plugin.send_email()
+
+        send_quota = conn.get_send_quota()
+        sent_count = int(send_quota['GetSendQuotaResponse']['GetSendQuotaResult']['SentLast24Hours'])
+        self.assertEqual(sent_count, 1)
+
