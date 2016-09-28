fc305cc5eb0b27837630767771dfe7548e01e27b
Sebastian Spoerer <sebastian.spoerer@immobilienscout24.de>
2016-09-28 16:00:14 +0200

    new integration test for iam handler. creating and deleting intergation test user

---
+++ src/integrationtest/python/handler/iam_tests.py
@@ -0,0 +1,56 @@
+import unittest2
+import boto3
+from boto import iam
+from monocyte.handler import iam as iam_handler
+from mock import MagicMock
+from monocyte.handler import Resource
+
+
+class IamTests(unittest2.TestCase):
+
+    def setUp(self):
+        self.iam_handler = iam_handler.User(MagicMock)
+        self.iam_handler.dry_run = True
+
+    def _create_user(self, username):
+        iam = boto3.client('iam')
+        iam.create_user(UserName=username)
+
+    def _delete_user(self, username):
+        iam = boto3.client('iam')
+        iam.delete_user(UserName=username)
+
+    def _uniq(self, resources):
+        uniq_names = []
+        for resource in resources:
+            name = resource.wrapped['UserName']
+            if not name.startswith('integrationtest'):
+                continue
+            uniq_names.append(name)
+
+        return uniq_names
+
+    def test_no_user_no_unwanted_resource(self):
+        resources = self.iam_handler.fetch_unwanted_resources()
+        unwanted_users = self._uniq(resources)
+        self.assertAlmostEqual([], unwanted_users)
+
+    def test_one_user_found_one_created(self):
+        resources = self.iam_handler.fetch_unwanted_resources()
+        username = 'integrationtest_user_1'
+        self._create_user(username)
+        unwanted_users = self._uniq(resources)
+        self._delete_user(username)
+        self.assertEqual(['integrationtest_user_1'], unwanted_users)
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest2.main()
