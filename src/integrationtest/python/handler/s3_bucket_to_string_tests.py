cd30779116fafadcd9bb44368727f910c144c643
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2017-01-04 13:49:57 +0100

    Avoid unneccessary call to self._uniq

--- src/integrationtest/python/handler/s3_bucket_to_string_tests.py
+++ src/integrationtest/python/handler/s3_bucket_to_string_tests.py
@@ -4,16 +4,16 @@ import unittest2
 
 from s3_tests_base import S3TestsBase
 
+
 class S3BucketToStringTests(S3TestsBase):
     def test_bucket_to_string_dry_run_no_sigv4(self):
         self._create_bucket('test-bucket10', 'eu-west-1')
         self.s3_handler.dry_run = True
 
         resources = self.s3_handler.fetch_unwanted_resources()
-        uniq_resources = self._uniq(resources)
-        self.assertEqual(len(uniq_resources), 1)
+        self.assertEqual(len(resources), 1)
 
-        bucket_str = self.s3_handler.to_string(uniq_resources[0])
+        bucket_str = self.s3_handler.to_string(resources[0])
         self.assertIn('test-bucket10', bucket_str)
         self.assertIn('eu-west-1', bucket_str)
 
@@ -22,10 +22,9 @@ class S3BucketToStringTests(S3TestsBase):
         self.s3_handler.dry_run = True
 
         resources = self.s3_handler.fetch_unwanted_resources()
-        uniq_resources = self._uniq(resources)
-        self.assertEqual(len(uniq_resources), 1)
+        self.assertEqual(len(resources), 1)
 
-        bucket_str = self.s3_handler.to_string(uniq_resources[0])
+        bucket_str = self.s3_handler.to_string(resources[0])
         self.assertIn('test-bucket11', bucket_str)
         self.assertIn('eu-central-1', bucket_str)
 
@@ -34,10 +33,9 @@ class S3BucketToStringTests(S3TestsBase):
         self.s3_handler.dry_run = True
 
         resources = self.s3_handler.fetch_unwanted_resources()
-        uniq_resources = self._uniq(resources)
-        self.assertEqual(len(uniq_resources), 1)
+        self.assertEqual(len(resources), 1)
 
-        bucket_str = self.s3_handler.to_string(uniq_resources[0])
+        bucket_str = self.s3_handler.to_string(resources[0])
         self.assertIn('test.bucket12', bucket_str)
         self.assertIn('eu-central-1', bucket_str)
 
